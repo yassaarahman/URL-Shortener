067e8fd4b5144777353fdd1af0f473b27aa3976a
Muhammad Yassaa Rahman <yassaarahman@gmail.com>
2026-09-09 12:11:06 +0500

    Set up middleware and routes in app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from "express";
+import { route } from "./routes/routes.js";
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(route);
 
 app.listen(3000, () => {
   console.log(`Server listening on http://localhost:3000`);
